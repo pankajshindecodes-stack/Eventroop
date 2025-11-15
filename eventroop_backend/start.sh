63c6eb6582b2269ace14dd01bed2927126f6f51f
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2025-11-15 14:14:15 +0530

    update requirements

--- eventroop_backend/start.sh
+++ eventroop_backend/start.sh
@@ -3,8 +3,15 @@
 cd eventroop_backend
 
 echo "Running migrations..."
+
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py create_default_groups
 
 echo "Starting server..."
 gunicorn eventroop_backend.wsgi:application --bind 0.0.0.0:$PORT
+#!/bin/bash
+set -e
+
+
+
