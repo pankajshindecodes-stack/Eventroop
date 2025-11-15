e6e0d379b8e9cbc792a2dc6cf91a330a41f3a76e
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2025-11-15 12:31:24 +0530

    railway deployment

---
+++ eventroop_backend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd eventroop_backend
+
+echo "Running migrations..."
+python manage.py migrate
+python manage.py create_default_groups
+
+echo "Starting server..."
+gunicorn eventroop_backend.wsgi:application --bind 0.0.0.0:$PORT
