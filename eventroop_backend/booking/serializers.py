f36767f8e6e633e09c726094c5e1828affaa4d73
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2025-11-22 13:22:04 +0530

    photos apis fixed

--- eventroop_backend/booking/serializers.py
+++ eventroop_backend/booking/serializers.py
@@ -3,8 +3,9 @@ from venue_manager.models import Venue, Photos
 from venue_manager.serializers import PhotosSerializer
 
 class VenueSerializer(serializers.ModelSerializer):
-    photos_li  = PhotosSerializer(many=True, read_only=True)
+    photos  = PhotosSerializer(many=True, read_only=True)
 
     class Meta:
         model = Venue
         fields = "__all__"
+        
