2c3b61ad7d01999ee6d6dfa5822f96873f206526
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2025-12-30 12:04:30 +0530

    filter fix

--- eventroop_backend/eventroop_backend/settings.py
+++ eventroop_backend/eventroop_backend/settings.py
@@ -142,13 +142,17 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
 
-
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS':(
         'eventroop_backend.pagination.StandardResultsSetPagination'
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
     )
 
 }
