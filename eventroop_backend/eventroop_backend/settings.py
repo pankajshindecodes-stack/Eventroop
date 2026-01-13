bf34764ac6c6963c3975334a1c1d814212f141f1
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-01-13 12:40:11 +0530

    final fixes for debug settings dev

--- eventroop_backend/eventroop_backend/settings.py
+++ eventroop_backend/eventroop_backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ----------------- SECURITY -----------------
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
